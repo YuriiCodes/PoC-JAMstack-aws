5f7a9f19e25103c4fbaa70e32aa3bd1892aa838b
Vitalii Hryban <gryban.v.2001@gmail.com>
2022-09-01 18:31:52 +0300

    stepper

--- src/components/Stepper.jsx
+++ src/components/Stepper.jsx
@@ -2,35 +2,34 @@ export function Stepper(props) {
     return (
         <div className="stepper d-flex flex-column mt-5 ml-5 mt-5 justify-content-center mx-auto border-end" >
             <div className="d-flex mb-1">
-                <div className="d-flex flex-column pr-4 align-items-center">
+                <div className="d-flex flex-column pr-4 align-items-center p-2">
                     <div className="rounded-circle py-2 px-3 bg-primary text-white mb-1">1</div>
                     <div className="line h-100"></div>
                 </div>
                 <div>
-                    <h5 className="text-dark">Create your application respository</h5>
-                    <p className="lead text-muted pb-3">Choose your website name & create repository</p>
+                    <h5 className="text-dark">Choose your favourite laptop</h5>
+                    <p className="lead text-muted pb-3">Choose your favourite laptop brand and model</p>
                 </div>
             </div>
             <div className="d-flex mb-1">
-                <div className="d-flex flex-column pr-4 align-items-center">
+                <div className="d-flex flex-column pr-4 align-items-center p-2">
                     <div className="rounded-circle py-2 px-3 bg-primary text-white mb-1">2</div>
                     <div className="line h-100"></div>
                 </div>
                 <div>
-                    <h5 className="text-dark">Clone application respository</h5>
-                    <p className="lead text-muted pb-3">Go to your dashboard and clone Git respository from the url in
-                        the dashboard of your application</p>
+                    <h5 className="text-dark">Fill in our form</h5>
+                    <p className="lead text-muted pb-3">Enter your contact information along with your favourite laptop
+                    </p>
                 </div>
             </div>
             <div className="d-flex mb-1">
-                <div className="d-flex flex-column pr-4 align-items-center">
+                <div className="d-flex flex-column pr-4 align-items-center p-2">
                     <div className="rounded-circle py-2 px-3 bg-primary text-white mb-1">3</div>
                     <div className="line h-100 d-none"></div>
                 </div>
-                <div>
-                    <h5 className="text-dark">Make changes and push!</h5>
-                    <p className="lead text-muted pb-3">Now make changes to your application source code, test it then
-                        commit &amp; push</p>
+                <div > 
+                    <h5 className="text-dark">Enjoy!</h5>
+                    <p className="lead text-muted pb-3">Enjoy a modern quality laptop</p>
                 </div>
             </div>
         </div>
